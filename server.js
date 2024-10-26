4050db625c957ae90a0aa802c01d1272bbce8fb0
WylkerVictor <wylker111@gmail.com>
2024-10-26 16:01:59 -0300

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/database');
-const routes = require('./Routes');
+const routes = require('./routes');
 
 const app = express();
 app.use(bodyParser.json());
