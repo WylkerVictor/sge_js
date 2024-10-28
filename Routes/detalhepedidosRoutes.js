3be2f24baa94c97a9603900648c6df3aaad29504
WylkerVictor <wylker111@gmail.com>
2024-10-28 13:25:23 -0300

    Update

--- Routes/detalhepedidosRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const detalheController = require('../controllers/detalhepedidosController');
-
-router.post('/detalhes', detalheController.criarDetalhes);
-router.get('/detalhes', detalheController.todosDetalhes);
-
-
-module.exports = router;
